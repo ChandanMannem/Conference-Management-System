745e5b5a671d608e5a43e9d8459c5d5c68abdb38
Chandra <lavanyasriram@outlook.com>
2020-04-10 16:23:27 -0400

    initial commit

--- CMS/urls.py
+++ CMS/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf.urls import include
 
 urlpatterns = [
     path('paperview/', views.paper_view_service),
+    path('signup/', views.signup, name='signup'),
+    path('', include('django.contrib.auth.urls')),
+    path('explore/', views.conference_view, name='explore'),
+    path('conference/<slug:category>/', views.conference_view, name='conference'),
+    path('conference/<slug:user_name>/<slug:category>/', views.conference_view, name='conference'),
+    path('conference/<slug:user_name>/', views.conference_view, name='conference_user'),
+    path('CreateData/', views.Create_data)
 ]
